95934b49f8d9f450e98c36b976134f7e479fd23d
Oskar2323 <62959449+Oskar2323@users.noreply.github.com>
2021-04-07 22:38:17 +0200

    start to add the sound. not able to choose yet

--- app.js
+++ app.js
@@ -24,6 +24,30 @@ class DrumKit{
     repeat(){
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`); // with this is possible to select each b0 from the pad in index and then go to the next one
+        
+        // loop over the pads 
+        activeBars.forEach(bar => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2 `;
+            
+            // create the if statement that if is active need to play the sound
+            if (bar.classList.contains('active')){
+                           
+                // check which pad is active to choose the right sound
+                if (bar.classList.contains('kick-pad')){
+                    // set the current time of the audio to 0 in order to play more sound in a row
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains('snare-pad')){
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains('hihat-pad')){
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }   
+         });
         this.index++;
     }
 
@@ -43,6 +67,11 @@ const drumKit = new DrumKit();
 // change the color when become active
 drumKit.pads.forEach(pad =>{
     pad.addEventListener('click', drumKit.activePad);
+
+    // to create the animation loop
+    pad.addEventListener('animationend', function(){
+        this.style.animation = "";
+    })
 });
 
 drumKit.playBtn.addEventListener('click', function(){
