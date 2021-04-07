07e1b83af6eaf764b1454c7fc2bae9a063c8444c
Oskar2323 <62959449+Oskar2323@users.noreply.github.com>
2021-04-07 22:00:59 +0200

    start JS with the pads

---
+++ app.js
@@ -0,0 +1,51 @@
+// create the class
+class DrumKit{
+    constructor(){
+        // add property
+        this.pads = document.querySelectorAll('.pad');
+
+        // the play button
+        this.playBtn = document.querySelector('.play');
+
+        // here it grab the audio from the hmtl file, to understand look for kick-sound 
+        this.kickAudio = document.querySelector('.kick-sound');
+        this.snareAudio = document.querySelector('.snare-sound');
+        this.hihatAudio = document.querySelector('.hihat-sound');
+        this.index = 0; // is the pad number 0, the pads are numbered from 0-7 and is usefull for the loop
+        this.bpm = 150; // create the bpm
+    }
+
+    // active pad
+    activePad(){
+        this.classList.toggle('active');  //add the class active
+    }
+
+    // create the loop 
+    repeat(){
+        let step = this.index % 8;
+        const activeBars = document.querySelectorAll(`.b${step}`); // with this is possible to select each b0 from the pad in index and then go to the next one
+        this.index++;
+    }
+
+    start(){
+        const interval = (60/this.bpm) * 1000; // times 1000 because the time is in ms
+        setInterval(() => {
+            this.repeat();
+        }, interval )  //the time is in ms
+    }
+}
+
+
+
+
+const drumKit = new DrumKit();
+
+// change the color when become active
+drumKit.pads.forEach(pad =>{
+    pad.addEventListener('click', drumKit.activePad);
+});
+
+drumKit.playBtn.addEventListener('click', function(){
+    drumKit.start();
+});
+ 
